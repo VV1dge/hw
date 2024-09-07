99f33896cf57c3f2d01796d92c3e1585f4168c5f
vv1dge <milov.v08@gmail.com>
2024-09-07 21:30:24 +0300

    chess5

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -7,12 +7,12 @@ func main() {
 	fmt.Println("Введите значение по горизонтали")
 	_, err := fmt.Scanf("%d", &x)
 	if err != nil {
-		fmt.Print(err)
+		fmt.Printf("Error: %v", err)
 	}
 	fmt.Println("Введите значение по вертикали")
 	_, err = fmt.Scanf("%d", &y)
 	if err != nil {
-		fmt.Print(err)
+		fmt.Printf("Error: %v", err)
 	}
 	for i := 1; i <= x; i++ { // проходимся циклом по строкам
 		for j := 1; j <= y; j++ { // проходмся циклом по столбцам
